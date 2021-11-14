c8acbe859256945d9b982de6f8abead0106eb3b7
santy <santiagoramirezm@ucundinamarca.edu.co>
2021-11-14 10:30:34 -0500

    organizacionbase

--- src/main/java/BaseDatos/Conexion.java
+++ src/main/java/BaseDatos/Conexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VPrincipal;
+package BaseDatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
